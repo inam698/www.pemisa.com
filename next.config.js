4c6d16d62517f07d2d6718be12efa480a6849561
inam698 <inam698@github.com>
2026-03-07 19:34:33 +0200

    Fix CSP: add style-src-elem, trim env vars to remove trailing newlines

--- next.config.js
+++ next.config.js
@@ -37,8 +37,16 @@ const nextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self' data:;",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://*.firebaseio.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' data: https://fonts.gstatic.com",
+              "img-src 'self' data: blob: https://*.googleapis.com https://*.googleusercontent.com",
+              "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com wss://*.firebaseio.com",
+              "frame-src 'self' https://*.firebaseapp.com https://*.firebaseio.com",
+            ].join("; "),
           },
         ],
       },
